63c0f411d722ea06c74e2f9992f3fe03a12841d1
dotdash <xxyxzxxyzyxxxzxyzxyxz@gmail.com>
2022-02-05 21:50:39 +0900

    update

---
+++ rust/thebook/project/branches/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index = index + 1;
+    }
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number)
+    }
+    println!("LIFTOFF!!!");
+}
